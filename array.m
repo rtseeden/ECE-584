0132f292bbb75b09b105f67d9f1064f071703f37
rtseeden <rtseeden@email.arizona.edu>
2020-04-10 17:28:27 -0700

    Add files via upload

--- array.m
+++ array.m
@@ -2,11 +2,10 @@ function [theta,array_factor_dB] = array(N,amp,phase,spacing)
     lambda = 1;%lambda is arbitrary because it cancels out
     d = lambda*spacing;
     kd = ((2*pi)/lambda)*d;
-    beta = 0;
     theta = 0:pi/1024:pi;
     array_factor = zeros(1,length(theta));
     for n = 1:1:N %using complex exp
-          array_factor = array_factor +exp(1i*(n-1)*(kd*cos(theta)));
+          array_factor = array_factor +amp(n)*exp(1i*phase(n))*exp(1i*(n-1)*(kd*cos(theta)));
     end
     array_factor_dB = 20*log10(abs(array_factor)/max(abs(array_factor)));
 end
--- main.m
+++ main.m
@@ -1,7 +1,7 @@
 spacing = 1/2; %wavelengths
 N = [8 19];% elements                                         
-amp=ones(2,20);
-phase=zeros(2,20);
+amp=ones(1,20);
+phase=zeros(1,20);
 %Part 2
 figure(1)
 [theta,array_factor_dB] = array(N(1),amp,phase,spacing);                    
@@ -20,29 +20,72 @@ xlabel('\Theta (degrees)'); ylabel('Array Factor (dB)');
 dir = directivity(theta,array_factor_dB)                                
 bw_3dB = BW_3dB(theta,array_factor_dB)                                     
 sidepeak = side_lobes(array_factor_dB)          
-
+%%
 %Part 3
-AF_cheb=chebwin(19,25)
-
+figure(3)
+amp=chebwin(19,25)
+[theta,array_factor_dB] = array(N(2),amp,phase,spacing);                    
+plot(rad2deg(theta),array_factor_dB); grid on;                              
+xlim([0 180]); ylim([-60 0]); title('19-element Dolph Chebyshev, {\lambda}/2 spacing'); 
+xlabel('\Theta (degrees)'); ylabel('Array Factor (dB)');                    
+dir = directivity(theta,array_factor_dB)                                
+bw_3dB = BW_3dB(theta,array_factor_dB)                                     
+sidepeak = side_lobes(array_factor_dB)
+%%
 %Part 4
 %ULA lambda/4 spacing
-figure(3)
+figure(4)
 spacing =1/4;
+amp=ones(1,20);
 [theta,array_factor_dB] = array(N(2),amp,phase,spacing)
 plot(rad2deg(theta),array_factor_dB); grid on;                              
 xlim([0 180]); ylim([-60 0]); title('19-element ULA, {\lambda}/4 spacing');
 xlabel('\Theta (degrees)'); ylabel('Array Factor (dB)');    
 
 %Dolph lambda/4 spacing
+figure(5)
+spacing =1/4;
+amp=chebwin(19,25);
+[theta,array_factor_dB] = array(N(2),amp,phase,spacing);                    
+plot(rad2deg(theta),array_factor_dB); grid on;                              
+xlim([0 180]); ylim([-60 0]); title('19-element Dolph Chebyshev, {\lambda}/4 spacing'); 
+xlabel('\Theta (degrees)'); ylabel('Array Factor (dB)');                    
+dir = directivity(theta,array_factor_dB)                                
+bw_3dB = BW_3dB(theta,array_factor_dB)                                     
+sidepeak = side_lobes(array_factor_dB)
 
 
 %ULA lambda spacing
-figure(5)
+figure(6)
 spacing =1;
+amp=ones(1,20);
 [theta,array_factor_dB] = array(N(2),amp,phase,spacing)
 plot(rad2deg(theta),array_factor_dB); grid on;                              
 xlim([0 180]); ylim([-60 0]); title('19-element ULA, {\lambda} spacing');
 xlabel('\Theta (degrees)'); ylabel('Array Factor (dB)');    
 
+%Dolph Lambda spacing
+figure(7)
+spacing =1;
+amp=chebwin(19,25);
+[theta,array_factor_dB] = array(N(2),amp,phase,spacing);                    
+plot(rad2deg(theta),array_factor_dB); grid on;                              
+xlim([0 180]); ylim([-60 0]); title('19-element Dolph Chebyshev, {\lambda} spacing'); 
+xlabel('\Theta (degrees)'); ylabel('Array Factor (dB)');                    
+dir = directivity(theta,array_factor_dB)                                
+bw_3dB = BW_3dB(theta,array_factor_dB)                                     
+sidepeak = side_lobes(array_factor_dB)
+%%
 %Part 5
+figure(8)
+phase=[pi 0 pi 0 pi 0 pi 0 pi 0 pi 0 pi 0 pi 0 pi 0 pi 0 pi];
+spacing =1/2;
+amp=chebwin(19,25);
+[theta,array_factor_dB] = array(N(2),amp,phase,spacing);                    
+plot(rad2deg(theta),array_factor_dB); grid on;                              
+xlim([0 180]); ylim([-60 0]); title('19-element Dolph Chebyshev with varying phase, {\lambda}/2 spacing'); 
+xlabel('\Theta (degrees)'); ylabel('Array Factor (dB)');                    
+dir = directivity(theta,array_factor_dB)                                
+bw_3dB = BW_3dB(theta,array_factor_dB)                                     
+sidepeak = side_lobes(array_factor_dB)
 
